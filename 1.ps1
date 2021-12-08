7f9df5f46548feacb7c7a715042b89c60611a184
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2021-12-08 13:29:02 +0100

    Create 1.ps1

---
+++ 1.ps1
@@ -0,0 +1,28 @@
+{
+
+    Param(
+
+    [Parameter(Mandatory=$false)]
+
+    [Switch]$Color
+
+    )
+
+ 
+
+if($Color -eq $false)
+
+    {
+
+        Write-Host "Hi from false"
+
+    }
+
+elseif($Color -eq $true)
+
+    {
+
+        Write-Host "Hi from true" -ForegroundColor Green
+
+    }
+
